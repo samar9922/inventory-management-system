e58cab09aebce90b990ca7655d970677e4f145f9
Samar Al Hadhrami <smrmd18@gmail.com>
2025-05-25 11:54:58 +0400

    Added price details to inventory report

--- InventoryReport.java
+++ InventoryReport.java
@@ -1,15 +1,16 @@
 public class InventoryReport {
     public static void main(String[] args) {
         String[] items = {"Mouse", "Keyboard", "Monitor"};
-        
+        double[] prices = {5.5, 12.0, 80.0};
+
         for (int i = 0; i < items.length; i++) {
             int reportType = i % 2;
             switch (reportType) {
                 case 0:
-                    System.out.println("Stock Report for: " + items[i]);
+                    System.out.println("Stock Report - Item: " + items[i] + ", Price: " + prices[i]);
                     break;
                 case 1:
-                    System.out.println("Sales Report for: " + items[i]);
+                    System.out.println("Sales Report - Item: " + items[i] + ", Price: " + prices[i]);
                     break;
                 default:
                     System.out.println("Invalid Report Type");
